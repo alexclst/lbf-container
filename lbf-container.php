46d70852b30f640609d14c0ae73db3d4a1994f1f
Alexander Celeste <alex@tenseg.net>
2021-02-09 11:10:33 -0600

    works with local 5

--- lbf-container.php
+++ lbf-container.php
@@ -3,14 +3,14 @@
  * LBF Container
  * Created by Alexander Celeste, alex@tenseg.net
  * March 2018
- * 
+ *
  * This script takes input of the current working directory and your home directory, and will output
  * associated Docker comntainer ID for the Local by Flywheel site that the current working directory is part
  * of.
- * 
+ *
  * It loops over Local by Flywheel's sites.json file and returns the container ID for the folder you passed
  * in. Written in PHP because I was more comfortable working with JSON in PHP than Bash.
- * 
+ *
  * Intended for use with lbf.sh.
  */
 
@@ -18,24 +18,24 @@
 $cwd = $argv[1];
 
 // get all local site settings
-$local_sites = json_decode(file_get_contents($argv[2] . "/Library/Application Support/Local by Flywheel/sites.json"), true);
+$local_sites = json_decode( file_get_contents( $argv[2] . '/Library/Application Support/Local/sites.json' ), true );
 
 // recurse through directories looking for container
-function find_container($path, $site) {
-	$path_parts = pathinfo($path);
-	if ($path_parts['dirname'] == $site['path']) {
-		return $site['container'];
-	} else if ($path_parts['dirname'] != '/') {
-		return find_container($path_parts['dirname'], $site);
+function find_container( $path, $site ) {
+	$path_parts = pathinfo( $path );
+	if ( $path_parts['dirname'] == $site['path'] ) {
+		return $site['id'];
+	} elseif ( '/' != $path_parts['dirname'] ) {
+		return find_container( $path_parts['dirname'], $site );
 	} else {
 		return null;
 	}
 }
 
 // loop all sites and create map to ids
-foreach($local_sites as $id => $site) {
-	$container = find_container("$cwd/trick", $site);
-	if ($container) {
+foreach ( $local_sites as $id => $site ) {
+	$container = find_container( "$cwd/trick", $site );
+	if ( $container ) {
 		echo $container;
 		return;
 	}
