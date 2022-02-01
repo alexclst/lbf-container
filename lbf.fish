d755352e403692e12a0706b4d36b67ba2d8e5283
efc <efc@clst.org>
2022-02-01 15:26:24 -0600

    add keepalive

--- lbf.fish
+++ lbf.fish
@@ -2,30 +2,30 @@
 
 # see if we were called naked
 if test -z "$argv[1]"
-	local-cli
-	exit
+    local-cli
+    exit
 end
 
 # see if we wanted help
 if test $argv[1] = 'help'
-	local-cli help
-	exit
+    local-cli help
+    exit
 end
 
 # ensure that Local is running before we attempt to do anything with it
 begin
-	pgrep Local
-end &> /dev/null
+    pgrep Local
+end &>/dev/null
 if test $status = 1 # Local isn't running
-	open -g /Applications/Local.app/
-	sleep 5
-	osascript -e 'tell application "System Events" to set visible of process "Local" to false'
+    open -g /Applications/Local.app/
+    sleep 5
+    osascript -e 'tell application "System Events" to set visible of process "Local" to false'
 end
 
 # see if we wanted to list sites
 if test $argv[1] = 'list-sites'
-	local-cli list-sites
-	exit
+    local-cli list-sites
+    exit
 end
 
 # get site id from container script
@@ -34,24 +34,50 @@ set SITE_ID (php  $SCRIPT_PATH-container/lbf-container.php $PWD $HOME "id")
 
 # all functions only make sense within Local site folders
 if test -z "$SITE_ID"
-	echo "This is not a Local site's folder."
-	exit
+    echo "This is not a Local site's folder."
+    exit
 end
 
 # check for custom restart command
 # TODO: remove once local-cli introduces this
 if test $argv[1] = 'restart'
-	local-cli 'stop-site' $SITE_ID
-	local-cli 'start-site' $SITE_ID
-	exit
+    local-cli 'stop-site' $SITE_ID
+    local-cli 'start-site' $SITE_ID
+    exit
 end
 
 # see if we were asked to launch the site
 # TODO: remove once local-cli introduces this
 if test $argv[1] = 'open'
-	set SITE_URL (php  $SCRIPT_PATH-container/lbf-container.php $PWD $HOME "domain")
-	open "http://$SITE_URL"
-	exit
+    set SITE_URL (php  $SCRIPT_PATH-container/lbf-container.php $PWD $HOME "domain")
+    open "http://$SITE_URL"
+    exit
+end
+
+# return the url of the site (note, always returns http)
+if test $argv[1] = 'url'
+    echo "http://"(php  $SCRIPT_PATH-container/lbf-container.php $PWD $HOME "domain")
+    exit
+end
+
+# keep a dying site alive
+if test $argv[1] = 'keepalive'
+    set SITE_URL "http://"(php  $SCRIPT_PATH-container/lbf-container.php $PWD $HOME "domain")
+    while true
+        curl -I --silent $SITE_URL >.tg-site-keepalive &
+        sleep 3
+        if test -s .tg-site-keepalive
+            rm .tg-site-keepalive
+            echo -ne '\r'(date)'                         '
+            sleep 10
+        else
+            kill $last_pid
+            echo -e "\r----------- "(date)" -----------"
+            local-cli 'stop-site' $SITE_ID
+            local-cli 'start-site' $SITE_ID
+        end
+    end
+    exit
 end
 
 # run the specified local-cli command with the found site id
